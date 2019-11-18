2e29b89cf7a42b0d6e0fa74ef000f8252a97ffea
yasuki0404 <yasuki.kanno@gmail.com>
2019-11-19 04:45:08 +0900

    グループにメッセージを表示する

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -20,7 +20,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_userr.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 
   def set_group
